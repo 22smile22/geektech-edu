314762958d2c62c07663ac219b7a4ed2d53ee569
22smile22 <96616073+22smile22@users.noreply.github.com>
2022-02-18 10:32:31 +0600

    django-ls4

--- data/django_14_lesson/shows/urls.py
+++ data/django_14_lesson/shows/urls.py
@@ -8,6 +8,9 @@ app_name = "shows"
 urlpatterns = [
     path("shows/", views.get_shows_all, name="shows_list"),
     path("shows/<int:id>/", views.get_show_detail, name="shows_detail"),
+    #ls4
+    path("shows/<int:id>/update/", views.put_shows_update, name="shows_update"),
+    path("shows/<int:id>/delete/", views.shows_delete, name="shows_delete"),
     #ls3
-    path("add-shows/", views.add_show, name="add_shows"),
+    path("add-shows/", views.add_show, name="add_show"),
 ]
